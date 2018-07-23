260a4843370806aa2f41b03bd06516602331228b
briannaliu <briannaliu3@gmail.com>
2018-07-23 21:18:28 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -28,3 +28,7 @@ puts "Lift right arm up and point right elbow diagonally to the right."
 puts "Raise right forearm so that it points straight up, and whip it in small circles to the beat of the song, as if you were a cowboy holding a rope lasso. This movement is repeated eight times as well"
 puts "Do"
 end
+
+def bust_a_move
+  puts "Do"
+end
